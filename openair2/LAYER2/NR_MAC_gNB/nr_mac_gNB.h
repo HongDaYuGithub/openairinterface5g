c09e19cbace48177c96bab46b9917bbdedf19d7d
Robert Schmidt <robert.schmidt@openairinterface.org>
2022-05-02 12:35:58 +0200

    Add MAC-RRC UL stubs for F1AP and direct

--- openair2/LAYER2/NR_MAC_gNB/nr_mac_gNB.h
+++ openair2/LAYER2/NR_MAC_gNB/nr_mac_gNB.h
@@ -60,6 +60,7 @@
 /* Interface */
 #include "nfapi_nr_interface_scf.h"
 #include "NR_PHY_INTERFACE/NR_IF_Module.h"
+#include "mac_rrc_ul.h"
 
 /* MAC */
 #include "LAYER2/MAC/mac.h"
@@ -701,6 +702,11 @@ typedef struct NR_bler_options {
   uint8_t max_mcs;
 } NR_bler_options_t;
 
+typedef struct nr_mac_rrc_ul_if_s {
+  /* TODO add other message types as necessary */
+  initial_ul_rrc_message_transfer_func_t initial_ul_rrc_message_transfer;
+} nr_mac_rrc_ul_if_t;
+
 /*! \brief UE list used by gNB to order UEs/CC for scheduling*/
 #define MAX_CSI_REPORTCONFIG 48
 typedef struct {
@@ -853,6 +859,8 @@ typedef struct gNB_MAC_INST_s {
   uint8_t harq_round_max;
   uint8_t min_grant_prb;
   uint8_t min_grant_mcs;
+
+  nr_mac_rrc_ul_if_t mac_rrc;
 } gNB_MAC_INST;
 
 #endif /*__LAYER2_NR_MAC_GNB_H__ */
