539852f98218182eb06f62310675e21180736e67
Marius Tillner <tillner@eurecom.fr>
2022-06-09 02:38:59 +0200

    fixed the missing ; after the latseq points

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_ulsch.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_ulsch.c
@@ -899,7 +899,7 @@ void nr_rx_sdu(const module_id_t gnb_mod_idP,
   }
 #if LATSEQ
     rx_lcid = ((NR_MAC_SUBHEADER_FIXED *)sduP)->LCID; // does this work for sduP instead of pduP too? 
-    LATSEQ_P("U mac.harq.up--mac.demux", "len%d:rnti%d:frame%d.slot%d.UE_id%d.lcid%d", sdu_lenP, current_rnti, frameP, slotP, UE_id, rx_lcid)
+    LATSEQ_P("U mac.harq.up--mac.demux", "len%d:rnti%d:frame%d.slot%d.UE_id%d.lcid%d", sdu_lenP, current_rnti, frameP, slotP, UE_id, rx_lcid);
 #endif    
   
 }
