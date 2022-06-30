ac1410ebe97ec08eb4273c6d374add516898c82e
Robert Schmidt <robert.schmidt@openairinterface.org>
2022-06-30 13:01:41 +0200

    Free GTP packet memory buffer after sending

--- openair3/ocp-gtpu/gtp_itf.cpp
+++ openair3/ocp-gtpu/gtp_itf.cpp
@@ -1195,6 +1195,7 @@ void *gtpv1uTask(void *args)  {
         case GTPV1U_GNB_TUNNEL_DATA_REQ: {
           gtpv1uSend2(compatInst(ITTI_MSG_DESTINATION_INSTANCE(message_p)),
                       &GTPV1U_GNB_TUNNEL_DATA_REQ(message_p), false, false);
+          itti_free(TASK_GTPV1_U, GTPV1U_GNB_TUNNEL_DATA_REQ(message_p).buffer);
         }
         break;
 
